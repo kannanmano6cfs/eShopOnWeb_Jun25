18ca8907f109c6644f54b49da6fb5cc69d0cddc2
Kannan Manoharan <161038652+kannanmano6cfs@users.noreply.github.com>
2025-06-10 10:26:42 +0530

    Update greetController.java

--- src/main/java/com/learning/exkubedemo/Controller/greetController.java
+++ src/main/java/com/learning/exkubedemo/Controller/greetController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class greetController {
-
+    //My changes
     @GetMapping("/")
     public String greet() {
         return "Hello World!! This is from container in pods of K8s Cluster";
