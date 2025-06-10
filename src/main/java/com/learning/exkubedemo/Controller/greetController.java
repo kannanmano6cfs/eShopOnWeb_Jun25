d2241b27ecf32fe6be0389f203795b6d2f4cc4cd
abc <abc@example.com>
2025-06-10 10:32:48 +0530

    Update in greetController by dev1

--- src/main/java/com/learning/exkubedemo/Controller/greetController.java
+++ src/main/java/com/learning/exkubedemo/Controller/greetController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class greetController {
     //My changes
+    //My change by dev1 branch
     @GetMapping("/")
     public String greet() {
         return "Hello World!! This is from container in pods of K8s Cluster";
